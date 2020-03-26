78e0603e7fb47b2059069816197da9576bf164d2
Abhinav Goel <goel39@purdue.edu>
2020-03-26 10:03:32 -0400

    updated downloader

--- downloader.sh
+++ downloader.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 DATETIME=`date +%Y-%m-%d_%H_%M_%S`
 echo="RUNNING!"
-n=0;
+n=$1;
+wc=($(wc -l streamlink_master_id.txt))
+num=${wc[0]}
+echo $num
 echo "$BASH_VERSION"
 readarray -t a < streamlink_master_id.txt
 while IFS="" read -r p || [ -n "$p" ]
 do
+       if [ $n -gt $num ]; then
+		break
+       fi
        echo ${a[n]}
        mkdir -p "${a[n]}"
+       echo $n
        timeout -sHUP 60s streamlink "$p" best -o "${a[n]}/$DATETIME.mp4"
-       n=$(($n+1))
+       n=$(($n+$2))
 done < streamlink_master_list.txt
 #EOF
