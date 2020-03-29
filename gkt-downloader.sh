155c01604d0e0e330f03596f7fac28f88c1ffff6
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-03-29 15:38:23 +0000

    simple wrapper for ensuring virtual env is sourced and preserves existing downloader.sh interface

---
+++ gkt-downloader.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is only to check for George's Anaconda CAM2 environment at ANL
+if [ -x "$(command -v conda)" ]; then
+  if [ "$(conda env list | grep ^cam2)" != "" ]; then 
+      echo "Found GKT environment at ANL."
+      source activate cam2
+   fi
+fi
+
+# This makes sure we have the required args
+if [ "$#" -lt 2 ]; then
+  echo "usage: ./downloader.sh part number-of-parts"
+  exit 1
+fi
+
+part=$1
+num_parts=$2
+download_to=/projects/SE_HPC/downloaded-videos
+
+python3 downloader.py --download-to "$download_to" --number-of-parts "$num_parts" --part-number "$part"
