93ba0993c9e5f80d2d924b859adb938a3e5a7dd1
ishaghodgaonkar <ighodgao@purdue.edu>
2020-04-24 18:34:46 -0400

    added multiple tries

---
+++ downloader_v2.py
@@ -0,0 +1,69 @@
+import os
+import time
+import sys
+import datetime
+from subprocess import call
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description='Covid19 video downloader')
+parser.add_argument('--part-number', dest='M', type = int,
+                    default=0,
+                    help='part number (M)')
+
+parser.add_argument('--number-of-parts', dest='N', type = int,
+                    default=8,
+                    help='number of parts (N)')
+
+parser.add_argument('--download-to', dest='path',
+                    default='.',
+                    help='download path')
+
+parser.add_argument('--remove-failed', dest='remove',
+                    default=True,
+                    help='delete failed downloads')
+parser.add_argument('--num_tries', dest='tries', default=10, help='number of tries to retry download if file size 0 or not downloaded')
+
+args = parser.parse_args()
+print(args)
+
+with open('streamlink_master_id.txt') as f:
+    ids = f.read().splitlines()
+
+with open('streamlink_master_list.txt') as f:
+    links = f.read().splitlines()
+
+id_map = dict(zip(ids, links))
+
+errorfile = open('errorfile.txt', 'a+')
+
+index = args.M
+while index < len(ids):
+        key = ids[index]
+        print(index+1, key)
+        try:
+            os.makedirs(args.path+'/'+key)
+        except FileExistsError:
+            pass
+        now = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
+        success = False
+        for i in range(args.tries):
+
+            call("timeout -sHUP 60s streamlink \""+id_map[key]+"\" best -o "+args.path+"/"+key+"/"+now+".mp4", shell = True)
+            if args.remove:
+                try:
+                    filesize = Path(args.path+"/"+key+"/"+now+".mp4").stat().st_size
+                    if filesize == 0:
+                        print(args.path+"/"+key+"/"+now+".mp4:", "File size 0. File deleted.", file = errorfile)
+                        os.unlink(args.path+"/"+key+"/"+now+".mp4")
+                    else:
+                        success = True
+                except:
+                    print(args.path+"/"+key+"/"+now+".mp4:", "File not downloaded.", file = errorfile)
+                    if os.path.exists(args.path+"/"+key) and os.path.isdir(args.path+"/"+key):
+                        if not os.listdir(args.path+"/"+key):
+                            os.rmdir(args.path+'/'+key)
+            if success:
+                break
+            time.sleep(2)
+        index += args.N
